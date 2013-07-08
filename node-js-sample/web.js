60a1f58a886256ff73410e1c0f835c8945fa772e
Ubuntu <ubuntu@ip-10-164-2-117.ec2.internal>
2013-07-08 01:12:49 +0000

    modified to read from index

--- node-js-sample/web.js
+++ node-js-sample/web.js
@@ -1,9 +1,15 @@
+var fs = require('fs');
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
+//var buffer = new Buffer(25);
+//response.send(fs.readFileSync("index.html","utf-8"), buffer.toString());
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(fs.readFileSync("index.html","utf-8"));
 });
 
 var port = process.env.PORT || 5000;
